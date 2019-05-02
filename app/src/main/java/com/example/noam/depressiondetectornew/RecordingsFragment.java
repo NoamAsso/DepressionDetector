a9eb123d0a5da0a007d19fd0c28401d3aec2ec4c
NoamAsso <assoulinenoam@gmail.com>
2019-05-02 14:03:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/noam/depressiondetectornew/RecordingsFragment.java
@@ -0,0 +1,27 @@
+package com.example.noam.depressiondetectornew;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RecordingsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RecordingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RecordingsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recordings, container, false);
+    }
+
+
+}
